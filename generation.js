c769f497366955e37b9c8f94072305519af72e76
Tausopy <122395389+Tausopy@users.noreply.github.com>
2023-08-31 17:52:16 +0200

    V2.1.1
    Ajout de la possibilité de personnalisé la longueur entre 12 et 22 caractères et ajout de la possibilité de mettre des caractères spéciaux.
    
    également :
    CSS séparer du fichier HTML

--- generation.js
+++ generation.js
@@ -1,30 +1,46 @@
-console.log('Génération lancée.');
+document.addEventListener("DOMContentLoaded", function() {
+    console.log('Génération lancée.');
 
-//Fonction pour générer un mot de passe aléatoire
-function generatePassword(length) {
-    const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-_*!/?,:";
-    let password = "";
-    for (let i = 0; i < length; i++) {
-        const randomIndex = Math.floor(Math.random() * charset.length);
-        password += charset.charAt(randomIndex);
+    // Fonction pour générer un mot de passe aléatoire
+    function generatePassword(length, useSpecialChars) {
+        const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        const charset_deux = "-_*!/?,:@#$%^&";
+        let selectedCharset = charset;
+
+        if (useSpecialChars) {
+            selectedCharset += charset_deux;
+        }
+
+        let password = "";
+        for (let i = 0; i < length; i++) {
+            const randomIndex = Math.floor(Math.random() * selectedCharset.length);
+            password += selectedCharset.charAt(randomIndex);
+        }
+        console.log('mot de passe généré.');
+        return password;
     }
-    console.log('mot de passe généré.');
-    return password;
-}
 
-// Fonction pour réagir au clic sur le bouton "Générer"
-document.getElementById("generateButton").addEventListener("click", function() {
-    const passwordLength = 18; // Longueur du mot de passe à générer
-    const generatedPassword = generatePassword(passwordLength);
+    // Fonction pour réagir au clic sur le bouton "Générer"
+    document.getElementById("generateButton").addEventListener("click", function() {
+        const passwordLengthInput = document.getElementById("passwordLengthInput");
+        const passwordLength = parseInt(passwordLengthInput.value);
 
-    // Copier le mot de passe dans le presse-papier
-    const tempInput = document.createElement("input");
-    document.body.appendChild(tempInput);
-    tempInput.value = generatedPassword;
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
+        const specialCharsCheckbox = document.querySelector('input[name="specialChars"]:checked');
+        const useSpecialChars = specialCharsCheckbox.value === 'specialChars_choice';
 
-    console.log('Mot de passe copié.');
+        const generatedPassword = generatePassword(passwordLength, useSpecialChars);
+        // Copier le mot de passe dans le presse-papier
+        const tempInput = document.createElement("input");
+        document.body.appendChild(tempInput);
+        tempInput.value = generatedPassword;
+        tempInput.select();
+        document.execCommand("copy");
+        document.body.removeChild(tempInput);
+
+        console.log('Mot de passe généré :', generatedPassword);
+    });
+
+
+
+
 });
-
